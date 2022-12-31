1b8acfe642f61d5a9d59be6597c396d060eaa7a8
munisht06 <munishtanwar77@gmail.com>
2022-12-31 18:34:57 -0500

    config changed

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
    * @type {import('next').NextConfig}
    */
 const nextConfig   = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
+  // images: {
+  //   loader: 'akamai',
+  //   path: '',
+  // },
   basePath: '/personal-portfolio',
   assetPrefix: './',
 };
