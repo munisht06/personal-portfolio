331c6c63d33311d276b24d9b8472670771cb1186
munisht06 <munishtanwar77@gmail.com>
2022-12-31 18:38:53 -0500

    config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-/**
-   * @type {import('next').NextConfig}
-   */
-const nextConfig   = {
-  // images: {
-  //   loader: 'akamai',
-  //   path: '',
-  // },
-  basePath: '/personal-portfolio',
-  assetPrefix: './',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "/personal-portfolio",
 };
 
-export default   nextConfig;
+module.exports = nextConfig;
