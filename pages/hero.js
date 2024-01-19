27e8b75d91a7c60c523d7d24c357af525997a007
munisht06 <munishtanwar77@gmail.com>
2024-01-18 23:44:46 -0500

    hero summary added

--- pages/hero.js
+++ pages/hero.js
@@ -11,16 +11,31 @@ export default function Hero() {
         <div className="text-center p-10 md:p-20">
           <h2 className="text-5xl py-2 text-gray-800 font-medium dark:text-gray-200 md:text-6xl">
             {" "}
-            Hi I&apos;m <span className="text-5xl py-2 text-teal-500 font-medium dark:text-teal-400 md:text-6xl">Munish Tanwar</span>
+            Hi I&apos;m{" "}
+            <span className="text-5xl py-2 text-teal-500 font-medium dark:text-teal-400 md:text-6xl">
+              Munish Tanwar
+            </span>
           </h2>
           <h3 className="text-2xl py-2 md:text-3xl dark:text-white ">
-            Graduate CS Student
+            Graduate CS Student and Developer
           </h3>
           <p className="text-md py-5 leading-8 text-gray-800 md:text-xl  dark:text-gray-200 max-w-lg mx-auto">
-            I am a graduate student at the University of Florida, majoring in
-            Computer Science. I am passionate about building web applications
-            and learning new technologies. I am currently looking for internship
-            opportunities for Summer 2023.
+            I'm a recent Computer Science graduate from the University of
+            Florida, skilled in full-stack development. Passionate about
+            creating scalable, user-friendly products. Actively seeking
+            full-time Software Engineer opportunities
+            {/* I am a graduate student at the University of Texas at Dallas
+            pursuing a Master&apos;s in Computer Science. I am a full-stack
+            developer with a passion for building products that are scalable and
+            user-friendly.
+
+            I am a recent Graduate Computer Science student from the University of
+            Florida. I am passionate about building things that live on the
+            internet, whether that be websites, applications, or anything in
+            between. I am currently looking for full-time opportunities as
+            a Software Engineer.
+
+            I am a recent graduate in Computer Science from the University of Florida, specializing in full-stack development. My enthusiasm lies in crafting scalable and user-friendly products that seamlessly blend innovation with functionality. Currently on the lookout for full-time opportunities as a Software Engineer, I bring a fresh perspective and a strong foundation in computer science to contribute to impactful projects */}
           </p>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
             <a
